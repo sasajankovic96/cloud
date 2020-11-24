7c046bd45193d6e2e538c09fe7fd5fda2d1f9652
sasajankovic96 <sasa.sale.jankovic.96@gmail.com>
2020-11-24 21:29:25 +0100

    Add env variable

--- cloud-demo/src/main/java/com/sasajankovic/clouddemo/HelloController.java
+++ cloud-demo/src/main/java/com/sasajankovic/clouddemo/HelloController.java
@@ -1,6 +1,8 @@
 package com.sasajankovic.clouddemo;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,10 +12,13 @@ public class HelloController {
     @Autowired
     private CounterRepository counterRepository;
 
+    @Value("${CUSTOM_ENV}")
+    private String customEnv;
+
     @GetMapping("hello")
-    public int hello() {
+    public String hello() {
         Counter counter = counterRepository.findById(1l).get().incrementCounter();
         counterRepository.save(counter);
-        return counter.getCounter();
+        return String.format("%s %d", customEnv, counter.getCounter());
     }
 }
